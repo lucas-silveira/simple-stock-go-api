75401343274637f129c1cb17c10769b6aebaf8c5
Lucas Silveira <lugano1995@gmail.com>
2021-02-03 16:11:44 -0300

    feat: add LogginHandler as Wrap routes

--- src/server.go
+++ src/server.go
@@ -3,8 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":3000", routes))
+	loggedRoutes := handlers.LoggingHandler(os.Stdout, routes)
+	log.Fatal(http.ListenAndServe(":3000", loggedRoutes))
 }
