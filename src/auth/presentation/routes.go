21c5e8a51d219fede7fe08e79aa2117325cc2c20
Lucas Silveira <lugano1995@gmail.com>
2021-02-03 14:20:19 -0300

    feat: create auth aggregation with routes file

---
+++ src/auth/presentation/routes.go
@@ -0,0 +1,17 @@
+package presentation
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Routes group all routes of auth aggregation
+func Routes(router *mux.Router) {
+	authRouter := router.PathPrefix("/auth").Subrouter()
+
+	authRouter.HandleFunc("/", func(resWriter http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(resWriter, "Auth Router")
+	}).Methods("GET")
+}
