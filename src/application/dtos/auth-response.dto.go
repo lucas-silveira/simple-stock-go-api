f8271ad6790ee9fd16d68ab73d083e359314e666
Lucas Silveira <lugano1995@gmail.com>
2021-02-04 02:18:16 -0300

    refactor: change token property name

--- src/application/dtos/auth-response.dto.go
+++ src/application/dtos/auth-response.dto.go
@@ -2,14 +2,14 @@ package dtos
 
 // AuthResponseDto ...
 type AuthResponseDto struct {
-	UserId int    `json:"user_id"`
-	Token  string `json:"token"`
+	UserId      int    `json:"user_id"`
+	AccessToken string `json:"access_token"`
 }
 
 // NewAuthResponseDto is a factory function that generate a new AuthResponseDto
 func NewAuthResponseDto(userID int, token string) *AuthResponseDto {
 	return &AuthResponseDto{
-		UserId: userID,
-		Token:  token,
+		UserId:      userID,
+		AccessToken: token,
 	}
 }
