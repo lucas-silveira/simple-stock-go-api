d7494f86cb9e3156c49b1b12a81bf3643c7b663d
Lucas Silveira <lugano1995@gmail.com>
2021-02-04 23:59:08 -0300

    feat: create a new user handler to create an user

--- src/presentation/http/handlers/users.handler.go
+++ src/presentation/http/handlers/users.handler.go
@@ -1,12 +1,32 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	. "main/src/application/controllers"
+	. "main/src/application/dtos"
 )
 
-// GetAllUsers handle get http request on users route and delivery a list of users
-func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+// GetUsers handle get http request on users route and delivery a list of users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Header.Get("userID"))
 	fmt.Fprintf(w, "Lista de usuários...")
+}
+
+// PostUsers handle post http request on users route and create a user
+func PostUsers(w http.ResponseWriter, r *http.Request) {
+	var createUserDto CreateUserDto
+	var userController UserController
+
+	json.NewDecoder(r.Body).Decode(&createUserDto)
+	err := userController.CreateAnUser(createUserDto)
+
+	if err != nil {
+		w.WriteHeader(err.StatusCode)
+		json.NewEncoder(w).Encode(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
