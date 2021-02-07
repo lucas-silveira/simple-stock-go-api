44c717291c1356d4bb18751466bb108fc6f9940d
Lucas Silveira <lugano1995@gmail.com>
2021-02-07 13:43:12 -0300

    refactor: create user dto validate

--- src/application/dtos/create-user.dto.go
+++ src/application/dtos/create-user.dto.go
@@ -5,5 +5,5 @@ type CreateUserDto struct {
 	Name            string `json:"name" validate:"required"`
 	Email           string `json:"email" validate:"required,email"`
 	Password        string `json:"password" validate:"required,min=6"`
-	ConfirmPassword string `json:"confirm_password" validate:"required,min=6"`
+	ConfirmPassword string `json:"confirm_password" validate:"required,min=6,eqfield=password"`
 }
