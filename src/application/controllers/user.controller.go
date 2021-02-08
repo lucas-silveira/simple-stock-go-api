8102c35ca1e610bf17c8e01404048e2d274fad1c
Lucas Silveira <lugano1995@gmail.com>
2021-02-08 19:53:02 -0300

    feat: implement repository into user controller

--- src/application/controllers/user.controller.go
+++ src/application/controllers/user.controller.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	. "main/src/application/dtos"
 	. "main/src/domain/user"
+	"main/src/infra/data/drivers"
+	"main/src/infra/data/repositories"
 	"main/src/infra/errors"
 	"main/src/infra/validator"
 	"net/http"
@@ -14,6 +16,7 @@ type UserController struct{}
 
 // CreateAnUser ...
 func (userController UserController) CreateAnUser(createUserDto CreateUserDto) *errors.Http {
+	var err error
 	errorsMessage, err := validator.Validate(createUserDto)
 
 	if err != nil {
@@ -24,12 +27,26 @@ func (userController UserController) CreateAnUser(createUserDto CreateUserDto) *
 		)
 	}
 
+	var userRepository IRepository
+
+	userRepository = repositories.NewUserRepository(drivers.DBConnection)
+
 	newUser := NewUser(
 		createUserDto.Name,
 		createUserDto.Email,
 		createUserDto.Password,
 	)
 	fmt.Println(newUser)
+	err = userRepository.Save(newUser)
+
+	if err != nil {
+		fmt.Println(err)
+		return errors.NewHttpError(
+			http.StatusInternalServerError,
+			"Internal server error.",
+			[]string{},
+		)
+	}
 
 	return nil
 }
