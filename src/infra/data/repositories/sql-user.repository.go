ce0ba88412ea55f4897de1d2460a2e4245b83cff
Lucas Silveira <lugano1995@gmail.com>
2021-02-08 23:40:13 -0300

    feat: improve save and create find by email methods into sql user repository

--- src/infra/data/repositories/sql-user.repository.go
+++ src/infra/data/repositories/sql-user.repository.go
@@ -16,12 +16,45 @@ func NewUserRepository(db *sql.DB) *User {
 	return &User{db}
 }
 
-// Save persist user object into database
-func (userRepo User) Save(user user.User) error {
-	_, err := userRepo.db.Query("INSERT INTO users (name, email, password) VALUES($1, $2, $3)", user.Name, user.Email, user.Password)
+// FindByEmail find a user object from database by email
+func (userRepo User) FindByEmail(email string) (*user.User, error) {
+	var user user.User
+	selectError :=
+		userRepo.db.QueryRow(
+			"SELECT id, name, email, password FROM users WHERE email = $1",
+			email,
+		).Scan(&user.ID, &user.Name, &user.Email, &user.Password)
 
-	if err != nil {
-		return fmt.Errorf("There was an error persisting the user in the database, err: %s", err)
+	if selectError != nil {
+		if selectError != sql.ErrNoRows {
+			return nil, fmt.Errorf("There was an error to select user from id, err: %s", selectError)
+		}
+	}
+
+	return &user, nil
+}
+
+// Save user object into database
+func (userRepo User) Save(user user.User) error {
+	var id string
+	selectError := userRepo.db.QueryRow("SELECT id FROM users WHERE id = $1", user.ID).Scan(&id)
+
+	if selectError != nil && id == "" {
+		if selectError != sql.ErrNoRows {
+			return fmt.Errorf("There was an error to select user from id, err: %s", selectError)
+		}
+
+		_, insertError := userRepo.db.Query("INSERT INTO users (name, email, password) VALUES($1, $2, $3)", user.Name, user.Email, user.Password)
+
+		if insertError != nil {
+			return fmt.Errorf("There was an error creating the user in the database, err: %s", insertError)
+		}
+	}
+
+	_, updateError := userRepo.db.Query("UPDATE users SET name=$1, email=$2, password=$3 WHERE id=$4", user.Name, user.Email, user.Password, user.ID)
+
+	if updateError != nil {
+		return fmt.Errorf("There was an error updating the user in the database, err: %s", updateError)
 	}
 
 	return nil
