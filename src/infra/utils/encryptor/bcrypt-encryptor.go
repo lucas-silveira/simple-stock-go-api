50d450dca19105cd7f6e4834a0cae0ad199cdfcc
Lucas Silveira <lugano1995@gmail.com>
2021-02-10 00:05:55 -0300

    feat: create bcrypt encryptor

---
+++ src/infra/utils/encryptor/bcrypt-encryptor.go
@@ -0,0 +1,25 @@
+package encryptor
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// BcryptEncryptor is a object that implements a encryptor interface
+type BcryptEncryptor struct{}
+
+// CreateHash generate a new hash from string
+func (encryptor BcryptEncryptor) CreateHash(aString string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(aString), bcrypt.MinCost)
+	if err != nil {
+		return "", fmt.Errorf("Occurred an error while generate a new password hash, err: %s", err)
+	}
+
+	return string(hash), nil
+}
+
+// Validate a string with a hash
+func (encryptor BcryptEncryptor) Validate(aHash, aString string) (bool, error) {
+	return true, nil
+}
