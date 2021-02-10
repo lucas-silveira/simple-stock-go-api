a523d89af482a62b3d89695b86a5e453a10cdadf
Lucas Silveira <lugano1995@gmail.com>
2021-02-10 00:05:47 -0300

    feat: create encryptor interface

---
+++ src/domain/encryptor.interface.go
@@ -0,0 +1,7 @@
+package domain
+
+// IEncryptor is a interface for a encryptor object
+type IEncryptor interface {
+	CreateHash(aString string) (string, error)
+	Validate(aHash, aString string) (bool, error)
+}
