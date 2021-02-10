26284cc352f9c6a1f1d8c0c630560921ad7e14e0
Lucas Silveira <lugano1995@gmail.com>
2021-02-10 00:32:55 -0300

    refactor: encryptor interface

--- src/domain/encryptor.interface.go
+++ src/domain/encryptor.interface.go
@@ -3,5 +3,5 @@ package domain
 // IEncryptor is a interface for a encryptor object
 type IEncryptor interface {
 	CreateHash(aString string) (string, error)
-	Validate(aHash, aString string) (bool, error)
+	Validate(aHash, aString string) bool
 }
