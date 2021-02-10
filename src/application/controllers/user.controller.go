94accfc0c39ff9819870e8701e061caf1bf723a5
Lucas Silveira <lugano1995@gmail.com>
2021-02-10 00:06:06 -0300

    feat: use encryptor into user controller

--- src/application/controllers/user.controller.go
+++ src/application/controllers/user.controller.go
@@ -3,16 +3,26 @@ package controllers
 import (
 	"fmt"
 	. "main/src/application/dtos"
+	"main/src/domain"
 	. "main/src/domain/user"
 	"main/src/infra/data/drivers"
 	"main/src/infra/data/repositories"
 	"main/src/infra/errors"
+	"main/src/infra/utils/encryptor"
 	"main/src/infra/validator"
 	"net/http"
 )
 
 // UserController ...
-type UserController struct{}
+type UserController struct {
+	encryptor domain.IEncryptor
+}
+
+// NewUserController is a factory that return a new UserController object
+func NewUserController() UserController {
+	encryptor := &encryptor.BcryptEncryptor{}
+	return UserController{encryptor: encryptor}
+}
 
 // CreateAnUser ...
 func (userController UserController) CreateAnUser(createUserDto CreateUserDto) *errors.Http {
@@ -28,16 +38,8 @@ func (userController UserController) CreateAnUser(createUserDto CreateUserDto) *
 	}
 
 	var userRepository IRepository
-
 	userRepository = repositories.NewUserRepository(drivers.DBConnection)
-
-	newUser := NewUser(
-		createUserDto.Name,
-		createUserDto.Email,
-		createUserDto.Password,
-	)
-
-	userExists, err := userRepository.FindByEmail(newUser.Email)
+	userExists, err := userRepository.FindByEmail(createUserDto.Email)
 
 	if err != nil {
 		fmt.Println(err)
@@ -48,13 +50,30 @@ func (userController UserController) CreateAnUser(createUserDto CreateUserDto) *
 		)
 	}
 
-	if userExists != nil {
+	if *userExists != (User{}) {
 		return errors.NewHttpError(
 			http.StatusBadRequest,
 			"The user already exists.",
 			[]string{},
 		)
 	}
+
+	passwordHashed, err := userController.encryptor.CreateHash(createUserDto.Password)
+
+	if err != nil {
+		fmt.Println(err)
+		return errors.NewHttpError(
+			http.StatusInternalServerError,
+			"Internal server error.",
+			[]string{},
+		)
+	}
+
+	newUser := NewUser(
+		createUserDto.Name,
+		createUserDto.Email,
+		passwordHashed,
+	)
 
 	err = userRepository.Save(newUser)
 
